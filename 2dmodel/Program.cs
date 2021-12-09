c9552b9d6a8e32df9b3026aa451a0fd133d70890
user5-2 <user5-2@citp.energypk.ru>
2021-12-09 12:55:56 +0300

    Добавьте файлы проекта.

---
+++ 2dmodel/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2dmodel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите высоту: ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите ширину: ");
+            int width = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
